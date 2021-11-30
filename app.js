6b99849053e3b89ea158efdcddb1509356d9c586
NebelwerferT <NebelwerferT9@gmail.com>
2021-11-30 07:29:22 +0400

    refactor: remove unused id after switching to BEM according to rule 2.1 in html-and-css-extended.md

--- app.js
+++ app.js
@@ -8,7 +8,7 @@
 
 // Event handling, user interaction is what starts the code execution.
 
-var taskInput = document.getElementById("new-task");//Add a new task.
+var taskInput = document.querySelector(".new-task");//Add a new task.
 var addButton = document.querySelector(".btn_add");//first button
 var incompleteTaskHolder = document.querySelector(".tasks_incomplete");//ul of #incompleteTasks
 var completedTasksHolder = document.querySelector(".tasks_complete");//completed-tasks
